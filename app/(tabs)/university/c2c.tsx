e88d6ba9952628f601fe3624ece73579a3da042a
KEERTHANA MANOJ <keerthanamiami@gmail.com>
2026-03-30 00:48:35 +0530

    lalapp

--- app/(tabs)/university/c2c.tsx
+++ app/(tabs)/university/c2c.tsx
@@ -90,7 +90,7 @@ export default function C2C() {
   return (
     <View style={styles.container}>
       <Image
-        source={require("./splash.png")}
+        source={require("../assets/splash.png")}
         style={styles.logo}
         resizeMode="contain"
       />
