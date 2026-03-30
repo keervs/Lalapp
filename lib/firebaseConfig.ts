ee49419fd7bdfbf3453c13b9202db7107b3025bc
KEERTHANA MANOJ <keerthanamiami@gmail.com>
2026-03-30 10:19:30 +0530

    nynthusux

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,36 +1,20 @@
-// lib/firebaseConfig.ts
-
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { initializeApp } from "firebase/app";
-import { Auth, getAuth, initializeAuth } from "firebase/auth";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { initializeAuth, inMemoryPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO8-lDbbNh_hFi51ikbrQPpzLjxQS3Dcg",
+  apiKey: "AIzaSyAO8-lDbbNh_hgrbFi51ikbrQPpzLjxQS3Dcg",
   authDomain: "lalappp.firebaseapp.com",
-  projectId: "lalappp",
+  projectId: "lalattppp",
   storageBucket: "lalappp.firebasestorage.app",
   messagingSenderId: "71560906262",
-  appId: "1:71560906262:web:b42aa0718935dfc38c9737",
+  appId: "1:71560906262:web:b42aa0718935dfc9737"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-// ✅ THIS LINE FIXES YOUR ERROR
-let auth: Auth;
+export const auth = initializeAuth(app, {
+  persistence: inMemoryPersistence
+});
 
-try {
-  const { getReactNativePersistence } = require("firebase/auth/react-native");
-
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-
-  console.log("✅ Auth with persistence");
-} catch (e) {
-  auth = getAuth(app);
-  console.log("⚠️ Fallback auth");
-}
-
-export { auth };
 export const db = getFirestore(app);
