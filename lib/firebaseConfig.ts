8f09ba218a72129ad658de4837960864d645f728
KEERTHANA MANOJ <keerthanamiami@gmail.com>
2026-03-30 10:25:33 +0530

    nynthusux

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeAuth, inMemoryPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO8-lDbbNh_hgrbFi51ikbrQPpzLjxQS3Dcg",
+  apiKey: "AIzaSyAO8-lDbbNh_hFi51ikbrQPpzLjxQS3Dcg",
   authDomain: "lalappp.firebaseapp.com",
-  projectId: "lalattppp",
+  projectId: "lalappp",
   storageBucket: "lalappp.firebasestorage.app",
   messagingSenderId: "71560906262",
-  appId: "1:71560906262:web:b42aa0718935dfc9737"
+  appId: "1:71560906262:web:b42aa0718935dfc38c9737"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
